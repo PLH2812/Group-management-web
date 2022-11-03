a0765c059fb4ed1d907de42ae1f3d23dd76b911c
PLH2812 <75527929+PLH2812@users.noreply.github.com>
2022-11-03 11:57:54 +0700

    fix bug token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,18 +2,23 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const auth = async (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer ", "");
-  const data = jwt.verify(token, process.env.JWT_KEY);
   try {
-    const user = await User.findOne({ _id: data._id, "tokens.token": token });
-    if (!user) {
-      throw new Error();
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const data = jwt.verify(token, process.env.JWT_KEY);
+    try {
+      const user = await User.findOne({ _id: data._id, "tokens.token": token });
+      if (!user) {
+        throw new Error();
+      }
+      req.user = user;
+     req.token = token;
+     next();
+   } catch (error) {
+      res.status(401).send({ error: "Không được phép truy cập" });
     }
-    req.user = user;
-    req.token = token;
-    next();
   } catch (error) {
-    res.status(401).send({ error: "Not authorized to access this resource" });
+    res.status(500).send({ error:"Token không hợp lệ"});
   }
+  
 };
 module.exports = auth;
