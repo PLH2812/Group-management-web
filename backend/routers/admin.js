fa8731dbe662db5215dda68bae3b334c97d14b29
PLH2812 <75527929+PLH2812@users.noreply.github.com>
2022-11-20 00:17:22 +0700

    refactor all api routes to "/api/..."

--- backend/routers/admin.js
+++ backend/routers/admin.js
@@ -9,7 +9,7 @@ const adminRole = process.env.ROLE_ADMIN;
 
 /*--------------------------------------------------------------------------User Management Start--------------------------------------------------------------------------*/
 
-router.post("api/admin/createUser", requireRole(adminRole), async (req, res) => {
+router.post("/api/admin/createUser", requireRole(adminRole), async (req, res) => {
     // Create a new user
     try {
         const user = new User(req.body);
@@ -20,7 +20,7 @@ router.post("api/admin/createUser", requireRole(adminRole), async (req, res) => 
     }
 });
 
-router.post("api/admin/updateUser/:_id", requireRole(adminRole), async (req, res) => {    
+router.post("/api/admin/updateUser/:_id", requireRole(adminRole), async (req, res) => {    
     // Update an existing user
     try {
         const data = await User.findByIdAndUpdate(req.params['_id'], {
@@ -35,7 +35,7 @@ router.post("api/admin/updateUser/:_id", requireRole(adminRole), async (req, res
     }
 })
 
-router.post("api/admin/deleteUser/:_id", requireRole(adminRole), async (req, res) => {
+router.post("/api/admin/deleteUser/:_id", requireRole(adminRole), async (req, res) => {
     try {
         const data = await User.findByIdAndUpdate(req.params['_id'], {
             status: "DELETED" //Change status to "DELETED"
@@ -48,7 +48,7 @@ router.post("api/admin/deleteUser/:_id", requireRole(adminRole), async (req, res
     }
 })
 
-router.get("api/admin/getUser/:_id", requireRole(adminRole), async (req, res) => {
+router.get("/api/admin/getUser/:_id", requireRole(adminRole), async (req, res) => {
     try {
         const data = await User.findOne({_id: req.params['_id']});
         if(!data){
@@ -60,7 +60,7 @@ router.get("api/admin/getUser/:_id", requireRole(adminRole), async (req, res) =>
     }
 })
 
-router.get("api/admin/getAllUsers", requireRole(adminRole), async (req, res) => {
+router.get("/api/admin/getAllUsers", requireRole(adminRole), async (req, res) => {
     try {
         const data = await User.find();
         res.status(201).send(data);
