8de271f9be65e4be38bb4b246273103fca89bdbc
PLH2812 <75527929+PLH2812@users.noreply.github.com>
2023-07-08 22:29:53 +0700

    notification

---
+++ backend/models/Notification.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = mongoose.Schema({
+    userId:{
+        type: String,
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    readAt:{
+        type: Date
+    }
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
---
+++ backend/routers/user/notification.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const errorHandler = require("../../middleware/errorHandler")
+const {tryCatch} = require("../../utils/tryCatch")
+const Notification = require("../../models/Notification")
+const auth = require("../../middleware/auth").auth
+
+const router = express.Router();
+
+router.post("/api/users/createNotification", auth, tryCatch(async (req, res) => {
+    const notification = new Notification(req.body);
+    notification.save();
+    return res.status(200).send("Tạo thông báo thành công!");
+}))
+
+router.get("/api/users/getNotifications", auth, tryCatch(async (req, res) => {
+    const uid = req.user._id;
+    const notifications = await Notification.find({userId: uid}).exec();
+    return res.status(200).send(notifications);
+}))
+
+router.get("/api/users/getNotification/:notificationId", auth, tryCatch(async (req, res) => {
+    const notification = await Notification.findById(req.params.notificationId);
+    if (!notification) throw new Error("Không tìm thấy thông báo!");
+    notification.readAt = Date.now();
+    await notification.save();
+    return res.status(200).send(notification);
+}))
+
+router.patch("/api/users/updateNotification/:notificationId", tryCatch(async (req, res) => {
+    const filter = {_id: req.params.notificationId};
+    const update = req.body;
+    Notification.findOneAndUpdate(filter, update, (err, docs) => {
+        if (err) {
+            throw new Error(err);
+        }
+        else {
+            return res.status(200).send("Cập nhật thành công!");
+        }
+    })
+}))
+
+router.delete("/api/users/deleteNotification/:notificationId", auth, tryCatch(async (req, res) => {
+    const notificationId = req.params.notificationId;
+    Notification.findOneAndDelete({_id: notificationId }, function (err, docs) {
+        if (err){
+            throw new Error(err);
+        }
+        else{
+            return res.status(200).send("Xoá thành công thông báo: ", docs);
+        }
+    });
+}))
+
+router.use(errorHandler);
+
+module.exports = router;
