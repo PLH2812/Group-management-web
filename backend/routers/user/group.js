fa260c5b5b90615482d394c830a5cd1ad8acba6d
PLH2812 <75527929+PLH2812@users.noreply.github.com>
2023-07-10 18:59:26 +0700

    Update group.js

--- backend/routers/user/group.js
+++ backend/routers/user/group.js
@@ -80,13 +80,14 @@ router.get('/api/users/me/groups', auth, async(req, res, next) => {
       if (!group){
         res.status(400).send({ error: "Bạn không phải chủ nhóm!"});
       } else {
-        await Group.findByIdAndDelete(req.params['_id'], function (err, docs) {
+        const filter = {_id: req.params._id};
+        await Group.findOneAndDelete(filter, function (err, group) {
           if (err){
-            throw new Error(err);
-        }
+              throw new Error(err);
+          }
           else{
-            res.status(200).send({ message: "Xoá nhóm thành công!", deleted: docs});
-        }
+              return res.status(200).send({message:"Xoá nhóm thành công!", deleted: group});
+          }
         });
       }
     } catch (error) {
