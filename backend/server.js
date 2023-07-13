1b16970a03a76e053d4890bd6ed7672758e92c70
PLH2812 <75527929+PLH2812@users.noreply.github.com>
2023-07-13 10:39:01 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use(rootRouter);
+const appDir = dirname(require.main.filename)
+app.use('/images', express.static(appDir +  "/resources/uploads/"));
 
 
 
